5ea9f88d2d04ee642b8e85922de0617f59bbfa46
lucasadlerstein <lucasadlerstein@gmail.com>
2024-02-19 11:11:32 -0300

    navbar + audiogram

--- src/app/audiometria/nueva/page.js
+++ src/app/audiometria/nueva/page.js
@@ -1,10 +1,16 @@
+'use client';
+
 import React from 'react';
 import Layout from '@/app/components/general/Layout';
+import Identify from '@/app/components/patient/Identify';
+import {Container} from 'react-bootstrap';
+import Audiogram from '@/app/components/studies/audiogram/Audiogram';
 
 const AudiometriaNueva = () => {
     return (
         <Layout>
-            <p>Hola</p>
+            <Identify />
+            <Audiogram />
         </Layout>
     );
 }
--- src/app/components/general/Navigation.js
+++ src/app/components/general/Navigation.js
@@ -13,39 +13,58 @@ const Area = styled.div`
     background-attachment: fixed;
 `;
 
+const LinkNavigation = styled(Link)`
+    color: var(--primaryColor);
+    font-size: 18px;
+    margin-left: 10px;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin: auto 8px;
+
+`;
+
+const DropdownNavigation = styled(NavDropdown)`
+    margin: auto 0;
+    a {
+        color: var(--primaryColor)!important;
+        font-size: 18px!important;
+        text-align: right;
+    }
+
+`;
+
 const Navigation = () => {
     return (
         <Area>
-            <Navbar expand="lg" className="" sticky="top" >
+            <Navbar expand="lg" sticky="top" >
             <Container>
                 <Navbar.Brand href="#home" className="mr-auto">+Audigital</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse className='justify-content-end' id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                    <Link href="/home">
-                        <Button className="">INICIO</Button>
-                    </Link>
-                    <Link href="/ver-mas">
-                        <Button className="btn btn-secondary" style={{marginLeft: '5px'}}>VER MAS</Button>
-                    </Link>
-                    <NavDropdown style={{padding: '0 12px', marginLeft: '5px'}} className="btn btn-secondary" title="ESTUDIOS" id="basic-nav-dropdown">
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">AUDIOMETRÍA</NavDropdown.Item>
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">LOGOAUDIOMETRÍA</NavDropdown.Item>
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">TIMPANOMETRÍA</NavDropdown.Item>
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">IMPEDANCIOMETRÍA</NavDropdown.Item>
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">POTENCIALES EVOCADOS</NavDropdown.Item>
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">OTOEMISIONES</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown style={{padding: '0 12px', marginLeft: '5px'}} className="btn btn-secondary" title="CUESTIONARIOS" id="basic-nav-dropdown">
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">PEACH</NavDropdown.Item>
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">Sp-SSQ12</NavDropdown.Item>
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">Vanderbilt</NavDropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown style={{padding: '0 12px', marginLeft: '5px'}} className="btn btn-secondary" title={<img src={'/img/web/n_perfil_usuario.svg'} alt="Icono del perfil"  />} id="basic-nav-dropdown">
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">MIS PACIENTES</NavDropdown.Item>
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">MI CUENTA</NavDropdown.Item>
-                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria">CERRAR SESIÓN</NavDropdown.Item>
-                    </NavDropdown>
+                    <LinkNavigation href="/home">INICIO</LinkNavigation>
+                    <LinkNavigation href="/ver-mas">VER MAS</LinkNavigation>
+                    <DropdownNavigation align="end" title="ESTUDIOS">
+                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/audiometria/nueva">AUDIOMETRÍA</NavDropdown.Item>
+                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/logoaudiometria">LOGOAUDIOMETRÍA</NavDropdown.Item>
+                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/timpanometria">TIMPANOMETRÍA</NavDropdown.Item>
+                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/impedanciometria">IMPEDANCIOMETRÍA</NavDropdown.Item>
+                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/potenciales-evocados">POTENCIALES EVOCADOS</NavDropdown.Item>
+                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/otoemisiones">OTOEMISIONES</NavDropdown.Item>
+                    </DropdownNavigation>
+                    <DropdownNavigation align="end" title="CUESTIONARIOS">
+                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/peach">PEACH</NavDropdown.Item>
+                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/sp-ssq12">Sp-SSQ12</NavDropdown.Item>
+                        <NavDropdown.Item className='btn btn-secondary mb-1' href="/venderbilt">Vanderbilt</NavDropdown.Item>
+                    </DropdownNavigation>
+                    <DropdownNavigation
+                        align="end"
+                        title={<img src={'/img/web/n_perfil_usuario.svg'} alt="Icono del perfil"  />}
+                    >
+                            <NavDropdown.Item className='btn btn-secondary mb-1' href="/mis-pacientes">MIS PACIENTES</NavDropdown.Item>
+                            <NavDropdown.Item className='btn btn-secondary mb-1' href="/mi-cuenta">MI CUENTA</NavDropdown.Item>
+                            <NavDropdown.Item className='btn btn-secondary mb-1' href="/cerrar-sesion">CERRAR SESIÓN</NavDropdown.Item>
+                    </DropdownNavigation>
 
                 </Nav>
                 </Navbar.Collapse>
