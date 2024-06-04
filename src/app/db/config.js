21491a17da263666aabe358ef53faccece10ff3c
Ezequiel-H <horowitzeze@gmail.com>
2024-06-04 14:44:59 -0300

    feat: backend connection

---
+++ src/app/db/config.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-param-reassign */
+// axiosConfig.js
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_FULL_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// TODO: agregar validaciones
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error),
+// );
+
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response && error.response.status === 401) {
+//       console.log(error);
+//     }
+//     return Promise.reject(error);
+//   },
+// );
+
+export default axiosInstance;
---
+++ src/utils/date.js
@@ -0,0 +1,11 @@
+export const formatDate = (originalDate) => {
+  const date = new Date(originalDate);
+
+  const day = String(date.getUTCDate()).padStart(2, '0');
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const year = date.getUTCFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+
+export const a = () => console.log('object');
